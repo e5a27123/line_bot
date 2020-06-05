16160689f99c8f879246e7adf8d91c4ea12d615f
Art Yeh <e5a27123@gmail.com>
2020-06-05 17:06:19 +0800

    adj app.py

--- app.py
+++ app.py
@@ -47,8 +47,7 @@ def handle_message(event):
             sticker_id='52002747')
 
     line_bot_api.reply_message(
-        event.reply_token,
-        #TextSendMessage(text=req),
+        TextSendMessage(text=req),
         sticker_message)
 
 
